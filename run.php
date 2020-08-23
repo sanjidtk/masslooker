0bcb464e5e2559ea72584e7ddfca6540a96e51b8
sanjidtk <48551302+sanjidtk@users.noreply.github.com>
2020-08-23 11:34:43 +0530

    Update run.php

--- run.php
+++ run.php
@@ -17,7 +17,7 @@ echo "
 	
 	\n";
 echo "[o] $$$$$$$$$$ Auto  Story Viewer by Photolooz $$$$$$$$$$$$$$$$$$ [o]\n";
-echo "  $----$----$ Made by 🤑 @mohsanjid 🤑 $----$---$   \n\n";
+echo "  $----$----$ Author nthanfp Modified by 🤑 @mohsanjid 🤑 $----$---$   \n\n";
 echo " Subscribe my Youtube channel $ PhotoLooz $ for more videos --😍
  \n\n";
 
